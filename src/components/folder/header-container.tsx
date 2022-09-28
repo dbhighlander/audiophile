7f145dcfc5091b09569c14934519565d1c4fb816
David Burgess <david@aline.digital.co.uk>
2022-09-28 19:10:03 +0100

    first commit

---
+++ src/components/folder/header-container.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../containers/header/header";
+
+const HeaderContainer = () => {
+    
+
+    return (
+        <Header />
+    )
+}
+
+export default HeaderContainer
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/reset.css'
+import './css/colors.css'
+import './css/structure.css'
+
+import './css/style.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
