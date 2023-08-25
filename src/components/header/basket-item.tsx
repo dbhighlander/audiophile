fbfadc254194a1d4806e4289f106d5d98a162023
David Burgess <david@aline.digital.co.uk>
2023-08-25 14:02:09 +0100

    Retried the images fix

--- src/components/header/basket-item.tsx
+++ src/components/header/basket-item.tsx
@@ -10,7 +10,7 @@ const BasketItem = ({basketItem,qtyChangeHandler}:BasketItemProps) => {
         <div className="basket__item">
             
             <div className="basket__item--desc">
-                <img src={'../../assets/cart/image-'+basketItem.product.slug+'.jpg'} alt={basketItem.product.name} className="basket__item__image" />
+                <img src={'assets/cart/image-'+basketItem.product.slug+'.jpg'} alt={basketItem.product.name} className="basket__item__image" />
                 <div className="basket__item--info">
                     <div className="basket__item--name">{basketItem.product.name}</div>
                     <div className="basket__item--price">${basketItem.product.price}</div>
